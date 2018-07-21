68cd6a24bd770fcc4b7737a2d609f279c279bf0d
Illisidan <gi.jon.531@gmail.com>
2018-07-22 00:36:28 +0800

    Ascending

---
+++ Ascending Order/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int array, tmp;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter amount of integers: ");
+        array = input.nextInt();
+        int numbers[] = new int[array];
+        System.out.println("Input numbers to be sorted: ");
+            for (int i = 0; i < array; i++) {
+                for (int j = i+1; j < array; j++) {
+                    if (numbers[i] > numbers[j])
+                    {
+                        tmp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = tmp;
+                    }
+                }
+            }
+            System.out.println(Arrays.toString(numbers));
+
+    }
+}
